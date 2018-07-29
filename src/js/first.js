dcb1ebd0b948969e3c0ce119ec54e8b87f42a23d
ljh4365 <532114690@qq.com>
2018-07-29 17:31:44 +0800

    首页吸顶效果完成和底部已完善

--- src/js/first.js
+++ src/js/first.js
@@ -245,7 +245,33 @@ $(function(){
 	})	
 })
 
-
+//***************************底部**************************
+$(function(){
+	
+	//************语言选择******************
+	 $(".choose").hover(function(){
+		$(".language").css("display","block");
+	},function(){
+		$(".language").css("display","none");
+	})
+	  $(".language").hover(function(){
+		$(".language").css("display","block");
+	},function(){
+		$(".language").css("display","none");
+	})
+	 
+	 //************微博图标******************
+	 var aspan = $(".blog span");
+	 aspan.hover(function(){
+	 	$(this).css("background-position-y","-155px");
+	 },function(){
+	 	$(this).css("background-position-y","-129px");
+	 })
+	 
+	 //************微信四个图标******************
+	 
+	 
+})
 
 
 
